a7ac9ded592efd9f5abb804d10a0002162cc4ece
endergreen12 <endergreenjapan@gmail.com>
2024-09-02 19:53:41 +0900

    Implement CampaignSubStage

--- SCHALE.GameServer/Controllers/Api/ProtocolHandlers/Campaign.cs
+++ SCHALE.GameServer/Controllers/Api/ProtocolHandlers/Campaign.cs
@@ -55,6 +55,108 @@ namespace SCHALE.GameServer.Controllers.Api.ProtocolHandlers
             };
         }
 
+        [ProtocolHandler(Protocol.Campaign_EnterSubStage)]
+        public ResponsePacket EnterSubStage(CampaignEnterSubStageRequest req)
+        {
+            var account = sessionKeyService.GetAccount(req.SessionKey);
+            var campaignExcel = excelTableService.GetTable<CampaignStageExcelTable>().UnPack().DataList.Where(x => x.Id == req.StageUniqueId).ToList().First();
+
+            var costId = campaignExcel.StageEnterCostId;
+            var costAmount = campaignExcel.StageEnterCostAmount;
+            var currency = account.Currencies.First();
+            currency.CurrencyDict[(CurrencyTypes)costId] -= costAmount;
+            currency.UpdateTimeDict[(CurrencyTypes)costId] = DateTime.Now;
+
+            context.Entry(currency).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+            context.SaveChanges();
+
+            return new CampaignEnterSubStageResponse()
+            {
+                ParcelResultDB = new()
+                {
+                    AccountCurrencyDB = currency,
+                },
+                SaveDataDB = new()
+                {
+                    ContentType = ContentType.CampaignSubStage,
+                }
+            };
+        }
+
+        [ProtocolHandler(Protocol.Campaign_SubStageResult)]
+        public ResponsePacket SubStageResult(CampaignSubStageResultRequest req)
+        {
+            var account = sessionKeyService.GetAccount(req.SessionKey);
+            var currencies = account.Currencies.First();
+            var campaignChapterExcel = excelTableService.GetTable<CampaignChapterExcelTable>().UnPack().DataList
+                .Where(x => x.NormalCampaignStageId.Contains(req.Summary.StageId) || x.HardCampaignStageId.Contains(req.Summary.StageId) || x.NormalExtraStageId.Contains(req.Summary.StageId) || x.VeryHardCampaignStageId.Contains(req.Summary.StageId)).ToList().First();
+            var campaignExcel = excelTableService.GetTable<CampaignStageExcelTable>().UnPack().DataList.Where(x => x.Id == req.Summary.StageId).First();
+            CampaignStageHistoryDB historyDb = new();
+            ParcelResultDB parcelResultDb = new()
+            {
+                AccountCurrencyDB = currencies,
+                DisplaySequence = new()
+            };
+
+            if (!req.Summary.IsAbort && req.Summary.EndType == Common.MX.Logic.Battles.BattleEndType.Clear)
+            {
+                CampaignService.CalcStrategySkipStarGoals(historyDb, req.Summary);
+
+                if (account.CampaignStageHistories.Any(x => x.StageUniqueId == req.Summary.StageId))
+                {
+                    var existHistory = account.CampaignStageHistories.Where(x => x.StageUniqueId == req.Summary.StageId).First();
+
+                    existHistory.Star1Flag = existHistory.Star1Flag ? true : historyDb.Star1Flag;
+                    existHistory.Star2Flag = existHistory.Star2Flag ? true : historyDb.Star2Flag;
+                    existHistory.Star3Flag = existHistory.Star3Flag ? true : historyDb.Star3Flag;
+
+                    existHistory.TodayPlayCount += 1;
+
+                    existHistory.LastPlay = DateTime.Now;
+
+                    context.Entry(existHistory).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+
+                    historyDb = existHistory;
+                }
+                else
+                {
+                    account.CampaignStageHistories.Add(historyDb);
+                }
+            }
+            else
+            {
+                // Return currencies
+                var costId = campaignExcel.StageEnterCostId;
+                var costAmount = campaignExcel.StageEnterCostAmount;
+                currencies.CurrencyDict[(CurrencyTypes)costId] += costAmount;
+                currencies.UpdateTimeDict[(CurrencyTypes)costId] = DateTime.Now;
+
+                context.Entry(currencies).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+
+                parcelResultDb.DisplaySequence.Add(new()
+                {
+                    Amount = costAmount,
+                    Key = new()
+                    {
+                        Type = ParcelType.Currency,
+                        Id = costId
+                    }
+                });
+            }
+
+            context.SaveChanges();
+
+            return new CampaignSubStageResultResponse()
+            {
+                TacticRank = 0,
+                CampaignStageHistoryDB = historyDb,
+                LevelUpCharacterDBs = new(),
+                ParcelResultDB = parcelResultDb,
+                FirstClearReward = new(),
+                ThreeStarReward = new()
+            };
+        }
+
         [ProtocolHandler(Protocol.Campaign_EnterMainStageStrategySkip)]
         public ResponsePacket EnterMainStageStrategySkip(CampaignEnterMainStageStrategySkipRequest req)
         {
